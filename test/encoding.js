ca2782038e058f503a5d64c665bd06d483d75d98
James Halliday <mail@substack.net>
2015-06-26 15:28:07 -0700

    more test cases for valueEncoding in opts

--- test/encoding.js
+++ test/encoding.js
@@ -15,6 +15,43 @@ tape('add node', function (t) {
   })
 })
 
+tape('add node with encoding option', function (t) {
+  var hyper = hyperlog(memdb())
+
+  hyper.add(null, { msg: 'hello world' }, { valueEncoding: 'json' },
+  function (err, node) {
+    t.error(err)
+    t.ok(node.key, 'has key')
+    t.same(node.links, [])
+    t.same(node.value, { msg: 'hello world' })
+    t.end()
+  })
+})
+
+tape('append node', function (t) {
+  var hyper = hyperlog(memdb(), { valueEncoding: 'json' })
+
+  hyper.append({ msg: 'hello world' }, function (err, node) {
+    t.error(err)
+    t.ok(node.key, 'has key')
+    t.same(node.links, [])
+    t.same(node.value, { msg: 'hello world' })
+    t.end()
+  })
+})
+
+tape('append node with encoding option', function (t) {
+  var hyper = hyperlog(memdb())
+
+  hyper.append({ msg: 'hello world' }, { valueEncoding: 'json' }, function (err, node) {
+    t.error(err)
+    t.ok(node.key, 'has key')
+    t.same(node.links, [])
+    t.same(node.value, { msg: 'hello world' })
+    t.end()
+  })
+})
+
 tape('add node with links', function (t) {
   var hyper = hyperlog(memdb(), { valueEncoding: 'json' })
 
@@ -60,6 +97,62 @@ tape('heads', function (t) {
           })
         })
       })
+    })
+  })
+})
+
+tape('heads with encoding option', function (t) {
+  var hyper = hyperlog(memdb())
+
+  hyper.heads({ valueEncoding: 'json' }, function (err, heads) {
+    t.error(err)
+    t.same(heads, [], 'no heads yet')
+    hyper.add(null, 'a', { valueEncoding: 'json' }, function (err, node) {
+      t.error(err)
+      hyper.heads({ valueEncoding: 'json' }, function (err, heads) {
+        t.error(err)
+        t.same(heads, [node], 'has head')
+        hyper.add(node, 'b', { valueEncoding: 'json' }, function (err, node2) {
+          t.error(err)
+          hyper.heads({ valueEncoding: 'json' }, function (err, heads) {
+            t.error(err)
+            t.same(heads, [node2], 'new heads')
+            t.end()
+          })
+        })
+      })
+    })
+  })
+})
+
+tape('get', function (t) {
+  var hyper = hyperlog(memdb(), { valueEncoding: 'json' })
+
+  hyper.add(null, { msg: 'hello world' }, function (err, node) {
+    t.error(err)
+    t.ok(node.key, 'has key')
+    t.same(node.links, [])
+    t.same(node.value, { msg: 'hello world' })
+    hyper.get(node.key, function (err, node2) {
+      t.ifError(err)
+      t.same(node2.value, { msg: 'hello world' })
+      t.end()
+    })
+  })
+})
+
+tape('get with encoding option', function (t) {
+  var hyper = hyperlog(memdb())
+
+  hyper.add(null, { msg: 'hello world' }, { valueEncoding: 'json' }, function (err, node) {
+    t.error(err)
+    t.ok(node.key, 'has key')
+    t.same(node.links, [])
+    t.same(node.value, { msg: 'hello world' })
+    hyper.get(node.key, { valueEncoding: 'json' }, function (err, node2) {
+      t.ifError(err)
+      t.same(node2.value, { msg: 'hello world' })
+      t.end()
     })
   })
 })
